0b9697a41e444c09018f870bf3ad3ef996ea84e9
Bashayer770 <Beshow95@gmail.com>
2025-04-08 08:55:10 +0300

    Bashsyer

--- src/app/island-form/island-form.component.ts
+++ src/app/island-form/island-form.component.ts
@@ -1,14 +1,34 @@
 import { Component, Input } from '@angular/core';
 
 import { Island } from '../../data/islands';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-island-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './island-form.component.html',
   styleUrl: './island-form.component.css',
 })
 export class IslandFormComponent {
   @Input() island!: Island;
+  name: string = '';
+  phone: string = '';
+
+  onSubmit() {
+    const confirmed = confirm(
+      `Are you sure you want to book ${this.island.name} for ${this.name} (Phone: ${this.phone})?`
+    );
+
+    if (confirmed) {
+      this.island.visitors.push({
+        name: this.name,
+        phone: this.phone,
+      });
+
+      this.name = '';
+      this.phone = '';
+    }
+  }
 }
